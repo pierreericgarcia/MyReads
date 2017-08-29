c87447b51b587c87356d4238573ecef59bd0657f
Dokeezy <darkaosis@gmail.com>
2017-08-29 15:07:21 +0200

    React router implemented / BookSearch and BookShelf component created

---
+++ src/BookSearch.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class BookSearch extends Component {
+  render() {
+    return (
+      <div>Search</div>
+    )
+  }
+}
+
+export default BookSearch;
---
+++ src/BookShelf.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class BookShelf extends Component {
+  render() {
+    return (
+      <div>Shelf</div>
+    )
+  }
+}
+
+export default BookShelf;
